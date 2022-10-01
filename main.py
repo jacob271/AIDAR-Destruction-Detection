37a91e51001eb10ac1133d20734b81cc30f7c263
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-10-01 23:29:07 +0200

    cleanup fastapi

--- main.py
+++ main.py
@@ -2,34 +2,26 @@ import cv2
 import numpy as np
 from PIL import Image
 from fastapi.responses import StreamingResponse
-
-import segmentation as seg
 from fastapi import FastAPI
 from fastapi import File
 from fastapi import UploadFile
+import segmentation as seg
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.post("/seg_image")
-def get_image(file: UploadFile = File(...)):
+@app.post("/segment_image")
+def segment_image(file: UploadFile = File(...)):
     image = np.array(Image.open(file.file))
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     cv2.imwrite("image.jpg", image)
-
-    mask = seg.perform_segmentation("Model1_AttentionNetwork_500.h5", "image.jpg", True)
-
+    mask = seg.perform_segmentation("image.jpg", True)
     mask = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    image[mask == 255] = (36, 255, 12)
-
-    cv2.imwrite("mask2.png", image)
+    image[mask == 255] = (111, 114, 255)
+    cv2.imwrite("mask.png", image)
 
     def iterfile():
-        with open("mask2.png", mode="rb") as file_like:
+        with open("mask.png", mode="rb") as file_like:
             yield from file_like
 
     return StreamingResponse(iterfile(), media_type="image/png")
