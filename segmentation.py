ee811757e9e3d7c53f133cc0e0f66c3df63dd849
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-10-01 15:05:30 +0200

    add paths as arguments for perform_segmentation

--- segmentation.py
+++ segmentation.py
@@ -17,50 +17,53 @@ import results
 import utility
 
 
-def perform_segmentation(model_name, path_to_images, apply_crf):
+def perform_segmentation(model_name, path_to_images, mask_dir, apply_crf,
+                         crf_dir=os.path.join("predicted_masks", "crf_masks")):
     model_densenet = utility.load_denseNet()
 
-    modelpath = os.path.join("models", model_name)
+    model_path = os.path.join("models", model_name)
 
-    if os.path.exists(modelpath):
+    if os.path.exists(model_path):
 
         print("loading Trained model..")
         model = Network.model_attention()
-        model.load_weights(modelpath)
-        masks, nameslist = results.predictmask(path_to_test=path_to_images, denseModel=model_densenet, model=model,
-                                               patch_size=224, window_stride=64)
-        modelnamedir = model_name.split('.')[0]
-        maskdir = os.path.join("predicted_masks", modelnamedir)
-        if not (os.path.exists(maskdir)):
+        model.load_weights(model_path)
+        masks, names_list = results.predictmask(path_to_test=path_to_images, denseModel=model_densenet, model=model,
+                                                patch_size=224, window_stride=64)
+        if not (os.path.exists(mask_dir)):
             print("Making new directory for masks")
-            os.mkdir(maskdir)
+            os.mkdir(mask_dir)
         for i in range(len(masks)):
-            imgname = nameslist[i].split("/")[-1]
+            imgname = names_list[i].split("/")[-1]
             maskname = imgname.split(".")[0]
             mask = np.where(masks[i] > 0.6, 255, 0)
-            cv2.imwrite(os.path.join(maskdir, maskname + ".png"), mask)
+            cv2.imwrite(os.path.join(mask_dir, maskname + ".png"), mask)
 
         if apply_crf:
 
             print("Applying Crf...")
-            crfdir = crf.CRF(masks, nameslist)
+            crf.perform_crf(masks, names_list, crf_dir)
             print("masks generated.Check below directories")
-            print(crfdir)
-            print(maskdir)
+            print(crf_dir)
+            print(mask_dir)
+            return {"message": "Performed segmentation successfully", "dirs": [crf_dir, mask_dir]}
         else:
             print("masks generated.Check below directories")
-            print(maskdir)
+            print(mask_dir)
+            return {"message": "Performed segmentation successfully", "dirs": [mask_dir]}
     else:
         print("Error: Model path not found")
+        return {"message": "Model path not found"}
 
 
-if __name__ == "__main__":
-    parser = OptionParser()
-    parser.add_option("--model_name", dest="model_name", help="Path to Model to calculate accuracy of test data.")
-    parser.add_option("--test_path", dest="test_path", help="Path to test data.")
-    parser.add_option("--apply_CRF", dest="apply_CRF", help="Mention if you want to apply CRF 'yes' or 'no' ")
-    parser.add_option("--patch_stride", dest="patch_stride", type="int",
-                      help="mention stride of window to extract patches and features.", default=64)
-    (options, args) = parser.parse_args()
-
-    perform_segmentation(options.model_name, options.test_path, options.apply_CRF == "yes")
+#if __name__ == "__main__":
+#    parser = OptionParser()
+#    parser.add_option("--model_name", dest="model_name", help="Path to Model to calculate accuracy of test data.")
+#    parser.add_option("--test_path", dest="test_path", help="Path to test data.")
+#    parser.add_option("--apply_CRF", dest="apply_CRF", help="Mention if you want to apply CRF 'yes' or 'no' ")
+#    parser.add_option("--patch_stride", dest="patch_stride", type="int",
+#                      help="mention stride of window to extract patches and features.", default=64)
+#    (options, args) = parser.parse_args()
+#
+#    perform_segmentation(options.model_name, options.test_path,
+#                         os.path.join("predicted_masks", options.model_name.split('.')[0]), options.apply_CRF == "yes")
