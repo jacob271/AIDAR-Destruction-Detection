546a05b837af57245fb98c9c7917678642e157be
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-10-01 18:48:12 +0200

    change api call to return the segmentation mask

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from PIL import Image
+from fastapi.responses import StreamingResponse
 
 import segmentation as seg
 from fastapi import FastAPI
@@ -18,5 +19,14 @@ async def root():
 @app.post("/seg_image")
 def get_image(file: UploadFile = File(...)):
     image = np.array(Image.open(file.file))
-    cv2.imwrite(image)
-    return seg.perform_segmentation("Model1_AttentionNetwork_500.h5", ".", "masks", True, "crf_masks")
+    cv2.imwrite("image.jpg", image)
+
+    mask = seg.perform_segmentation("Model1_AttentionNetwork_500.h5", "image.jpg", True)
+
+    cv2.imwrite("mask.png", mask)
+
+    def iterfile():
+        with open("mask.png", mode="rb") as file_like:
+            yield from file_like
+
+    return StreamingResponse(iterfile(), media_type="image/png")
