8d0bd99ad6528058a8164d0e58d5a77d17634f7e
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-10-01 23:32:42 +0200

    fix the model

--- segmentation.py
+++ segmentation.py
@@ -1,54 +1,40 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 11 11:05:48 2019
-
-@author: hec
-"""
 import os
 from optparse import OptionParser
-
 import numpy as np
-
 import Network
 import crf
 import results
 import utility
+import cv2
 
 
-def perform_segmentation(model_name, path_to_image, apply_crf):
+def perform_segmentation(path_to_image, apply_crf):
     model_densenet = utility.load_denseNet()
+    model_path = os.path.join("models", "Model1_AttentionNetwork_500.h5")
 
-    model_path = os.path.join("models", model_name)
+    if not os.path.exists(model_path):
+        print("Error: model not available..")
 
-    if os.path.exists(model_path):
+    print("loading Trained model..")
+    model = Network.model_attention()
+    model.load_weights(model_path)
+    mask, name = results.predict_mask(path_to_image=path_to_image, denseModel=model_densenet, model=model,
+                                      patch_size=224, window_stride=64)
+    mask2 = np.where(mask > 0.6, 255, 0)
 
-        print("loading Trained model..")
-        model = Network.model_attention()
-        model.load_weights(model_path)
-        mask, name = results.predict_mask(path_to_image=path_to_image, denseModel=model_densenet, model=model,
-                                          patch_size=224, window_stride=64)
-        mask2 = np.where(mask > 0.6, 255, 0)
-
-        if apply_crf:
-
-            print("Applying Crf...")
-            crf_mask = crf.perform_crf(mask, path_to_image)
-            return crf_mask
-        else:
-            return mask2
+    if apply_crf:
+        print("Applying Crf...")
+        crf_mask = crf.perform_crf(mask, path_to_image)
+        return crf_mask
     else:
-        print("Error: Model path not found")
-        return
+        return mask2
 
 
 if __name__ == "__main__":
     parser = OptionParser()
-    parser.add_option("--model_name", dest="model_name", help="Path to Model to calculate accuracy of test data.")
-    parser.add_option("--test_path", dest="test_path", help="Path to test data.")
+    parser.add_option("--image_path", dest="image_path", help="Path to image.")
     parser.add_option("--apply_CRF", dest="apply_CRF", help="Mention if you want to apply CRF 'yes' or 'no' ")
-    parser.add_option("--patch_stride", dest="patch_stride", type="int",
-                      help="mention stride of window to extract patches and features.", default=64)
     (options, args) = parser.parse_args()
 
-    perform_segmentation(options.model_name, options.test_path, options.apply_CRF == "yes")
+    mask = perform_segmentation(options.test_path, options.apply_CRF == "yes")
+    cv2.imwrite("mask.png", mask)
